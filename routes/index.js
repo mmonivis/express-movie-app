a6620bacb9907e09a48fd12cf7049d52b1b2c106
Marissa Monivis <mmonivis@gmail.com>
2017-05-31 15:04:30 -0400

    Init commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('../config/config');
+
+// console.log(config);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    const apiBaseUrl = 'http://api.themoviedb.org/3';
+    const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey;
+    const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+    request.get(nowPlayingUrl,(error, response, movieData)=>{
+        var movieData = JSON.parse(movieData);
+        res.render('index', {
+            movieData: movieData.results,
+            imageBaseUrl: imageBaseUrl
+        });
+    });
+});
+
+module.exports = router;
