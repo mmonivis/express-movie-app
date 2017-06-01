4dbb65b13b2d04cbcaaa4d18499e4e3f889292d5
Marissa Monivis <mmonivis@gmail.com>
2017-06-01 14:59:46 -0400

    Added single-movie page and more API info

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,66 @@ var config = require('../config/config');
 
 // console.log(config);
 
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    const apiBaseUrl = 'http://api.themoviedb.org/3';
-    const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey;
-    const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
     request.get(nowPlayingUrl,(error, response, movieData)=>{
         var movieData = JSON.parse(movieData);
         res.render('index', {
             movieData: movieData.results,
-            imageBaseUrl: imageBaseUrl
+            imageBaseUrl: imageBaseUrl,
+            titleHeader: 'Now Playing'
         });
     });
+});
+
+router.get('/search',(req, res)=>{
+    res.send("The get search page");
+});
+
+router.post('/search',(req, res)=>{
+    // req.body is available because of the body-parser module
+    // body-parser module was installed when you created the express app
+    // req.body is where POSTED data will live
+    // res.json(req.body);
+    var termUserSearchedFor = req.body.searchString;
+    var searchUrl = apiBaseUrl + '/search/movie?query='+termUserSearchedFor+'&api_key='+config.apiKey;
+    request.get(searchUrl,(error,response,movieData)=>{
+        var movieData = JSON.parse(movieData);
+        // res.json(JSON.parse(movieData));
+        // console.log(movieData);
+        res.render('index', {
+            movieData: movieData.results,
+            imageBaseUrl: imageBaseUrl,
+            titleHeader: 'You searched for: ' + termUserSearchedFor
+        });
+    });
+    // res.send("The post search page");
+});
+
+router.get('/movie/:id',(req, res)=>{
+    // the route has a :id in it. A : means WILDCARD
+    // a wildcard is ANYTHING in that slot.
+    // all wildcards in routes are available in req.params
+    var thisMovieId = req.params.id;
+    // build the URL per the API docs
+    var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${config.apiKey}`;
+    // Use the request module to make an HTTP request
+    request.get(thisMovieUrl,(error, reponse, movieData)=>{
+        // Parse the response into JSON
+        var movieData = JSON.parse(movieData);
+        // First arg: the view file.
+        // Second param: obj to send the view file
+        res.render('single-movie',{
+            movieData: movieData,
+            imageBaseUrl: imageBaseUrl
+        });
+
+    });
+    // res.send(req.params.id);
 });
 
 module.exports = router;
